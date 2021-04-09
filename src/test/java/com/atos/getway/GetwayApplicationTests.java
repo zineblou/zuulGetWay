fde26a1f1e8a365eacabde2e572f9a449da3087e
zineb louzi <zineblouzii@gmail.com>
2021-04-09 17:27:11 +0200

    Initial commit

---
+++ src/test/java/com/atos/getway/GetwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atos.getway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetwayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
